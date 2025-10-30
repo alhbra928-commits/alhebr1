f5bcdc7feb0308b4f31a15d33f2235e2a2e83348
alhbra928-commits <alhbra928@gmail.com>
2025-10-30 23:36:44 +0300

    Updated version-manifest.json

--- src/modules/public/components/ModernRoyalPlatform.tsx
+++ src/modules/public/components/ModernRoyalPlatform.tsx
@@ -203,9 +203,6 @@ export function ModernRoyalPlatform({
         </header>
 
         {/* Fixed Buttons */}
-        {onBackToAdmin && (
-          <BackToAdminButton onBackToAdmin={onBackToAdmin} />
-        )}
         <AdminCrownButton
           onAdminLogin={onAdminLogin}
           onFarmOwnerLogin={onFarmOwnerLogin}
